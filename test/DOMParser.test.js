f273548da1e92acd5593cb5d4263ecdc9a42bd5d
Rafael Nowrotek <mailbenignware.com>
2019-01-19 17:52:09 +0100

    Create html scaffold if missing

--- test/DOMParser.test.js
+++ test/DOMParser.test.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const { strict: assert } = require('assert');
+const pretty = require('pretty');
 const { serializeToString } = require('./utils');
 
 const { DOMParser } = require('..');
@@ -10,8 +11,19 @@ describe('DOMParser', function() {
     it('parses html', () => {
       const source = fs.readFileSync(`${__dirname}/fixtures/example.html`, 'utf-8');
       const document = DOMParser.parseFromString(source, 'text/html');
+      const actual = pretty(serializeToString(document));
+      const expected = pretty(source);
 
-      assert.equal(serializeToString(document), source);
+      assert.equal(actual, expected);
+    });
+
+    it('creates scaffold', () => {
+      const source = fs.readFileSync(`${__dirname}/fixtures/partial.html`, 'utf-8');
+      const document = DOMParser.parseFromString(source, 'text/html');
+      const actual = pretty(serializeToString(document));
+      const expected = pretty(`<html><body>${source}</body></html>`);
+
+      assert.equal(actual, expected);
     });
   })
 });
